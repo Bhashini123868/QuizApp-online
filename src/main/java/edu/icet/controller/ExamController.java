ec77e6cbc3445477e21b6c74336f03997b9c92b1
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-03-15 21:06:45 +0530

    first commit

---
+++ src/main/java/edu/icet/controller/ExamController.java
@@ -0,0 +1,23 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Exam;
+import edu.icet.servise.ExamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/exam")
+@RequiredArgsConstructor
+public class ExamController {
+//    http://localhost:8080/exam/create-exam
+    final ExamService service;
+
+    @PostMapping("/create-exam")
+    public void createExam(@RequestBody Exam exam){
+        service.createExam(exam);
+    }
+    @GetMapping("/get-exam-by-course-id/{id}")
+    public Exam getExamByCourseId(@PathVariable Integer id){
+        return service.getExamByCourseId(id);
+    }
+}
